99e873692251fda80594352462e4a66eed627600
gj <gj.nidea@gmail.com>
2017-01-21 07:24:07 +0800

    fixed heartstring trail

---
+++ heartstrings/Assets/Scripts/circleStartScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class circleStartScript : MonoBehaviour {
+
+	float angle =0;
+	float speed = (2 * Mathf.PI) / 5;
+	float radius= 0.032f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		angle += speed*Time.deltaTime; //if you want to switch direction, use -= instead of +=
+		transform.Translate (Mathf.Cos(angle)*radius, Mathf.Sin(angle)*radius, 0);
+	}
+}
